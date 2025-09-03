e8268f5d593571134bc8649ff3e4b362a51a71c8
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-09-03 08:37:53 +0200

    feat: job 3 finish

--- jour1/src/main/java/com/example/demo/DemoApplication.java
+++ jour1/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,32 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.controller.HelloController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	public HelloController hi= new HelloController();
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+
 	}
+
+	@GetMapping("/")
+	public String sayHi(){
+		return hi.hello();
+	}
+
+
+
+
+
 
 }
