353b7a0b597a8d35248a0fb08369c707e8e7620f
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-09-03 15:57:21 +0200

    feat: job 4 and 5

--- jour1/src/main/java/com/example/controller/HelloController.java
+++ jour1/src/main/java/com/example/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,6 +14,19 @@ public class HelloController {
     public String hello(){
         return "Hello World!";
     }
+
+    @Value("${greeting.message}")
+    private String greetingMessage;
+
+    @Value("${greeting.tools}")
+    private String toolsMessage;
+
+    @GetMapping("/profile")
+    @ResponseBody
+    public String greet(){
+        return greetingMessage + toolsMessage;
+    }
+
 
 
 }
