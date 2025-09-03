ab1df6b34cebeb597c99dc7a3152f88db6e26b6f
eltigani-abdallah <eltigani.abdallah@laplateforme.io>
2025-09-03 15:36:20 +0200

    refactor: hello world now shows from the controller folder instead of a new instance

--- jour1/src/main/java/com/example/controller/HelloController.java
+++ jour1/src/main/java/com/example/controller/HelloController.java
@@ -1,20 +1,18 @@
 package com.example.controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+@Controller
 public class HelloController {
 
-
+    @GetMapping("/")
+    @ResponseBody
     public String hello(){
         return "Hello World!";
     }
 
 
-    /*public String hello(@RequestParam(value= "name", defaultValue="World") String name){
-        return String.format("Hello %s!", name);
-    }*/
 }
